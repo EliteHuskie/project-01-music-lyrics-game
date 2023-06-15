59dfacb2f5a78e5738b9cb4e45d7cd0a9255ed3c
EliteHuskie <stratton_10@hotmail.com>
2023-06-15 19:27:19 -0300

    Added JavaScript logic for musixmatch api to clean it up a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-//never gonna give you up, never gonna let you down! 
-//GET RICK ROLLED SON!!
-
+// Start of Spotify Web API ------------------------
 const clientId = '65db5ee62bd54c539ab09c21d723eec3';
 const clientSecret = 'dd48590eba584fa4b97c4f25d91dffad';
 const redirectUri = 'https://elitehuskie.github.io/project-01-music-lyrics-game/';
@@ -49,56 +47,67 @@ const authorizationCode = urlParams.get('code');
 if (authorizationCode) {
   handleAuthorizationCode(authorizationCode);
 }
+// Start of MusixMatch API -----------------------
+const apiKey = '3be68bfc0da7e2c5a81fff0c26329572';
+const trackId = '31409936';
 
-//const apiKey = '3be68bfc0da7e2c5a81fff0c26329572';
-//const trackId = '31409936'; 
-//fetch(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=${apiKey}`)
-  //.then(response => response.json())
-  //.then(data => {
-    
- //console.log(data);
-  //})
- // .catch(error => {
-    
-    //console.log(error);
- // });
+fetch(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // Your code for handling the response data here
+  })
+  .catch(error => {
+    console.error(error);
+    // Your error handling code here
+  });
 
-//these two lines convert the string words of the lyrics_body provided from 
-//the API as well as the users guesses into arrays
-//const user_lyrics_input = user_input.split(" ");
-//const lyrics_array = lyrics_body.split(" ");
+const player = {
+  name: 'placeHolder',
+  score: 0
+};
 
-//calculates the percentage of words guessed correctly
-//let words_correct = 0;
-//let words_incorrect = 0;
-//let words_entered = words_incorrect + words_correct;
-//let scorePercentage = ((words_correct/words_entered) * 100);
+// These two lines convert the string words of the lyrics_body provided from
+// the API as well as the user's guesses into arrays
+const user_lyrics_input = user_input.split(' ');
+const lyrics_array = lyrics_body.split(' ');
 
-//compares the lyrics of the user and the lyrics of the song, 
+// Calculates the percentage of words guessed correctly
+let words_correct = 0;
+let words_incorrect = 0;
+
+// Compares the lyrics of the user and the lyrics of the song
 // and prints green or red depending on whether they match
-//for (let i = 0; i < user_lyrics_input.length; i++) {
-  //  if (user_lyrics_input[i] === actual_lyrics[i]) {
-    //    user_lyrics_input[i].style.color = "green";
-      //  words_correct++
-        
-    //} else {
-      //  user_lyrics_input[i].style.color = "red";
-        //words_incorrect++
-    //}
-//};
+for (let i = 0; i < user_lyrics_input.length; i++) {
+  if (user_lyrics_input[i] === lyrics_array[i]) {
+    // Wrap the word in a span element and apply styles
+    user_lyrics_input[i] = `<span style="color: green;">${user_lyrics_input[i]}</span>`;
+    words_correct++;
+  } else {
+    // Wrap the word in a span element and apply styles
+    user_lyrics_input[i] = `<span style="color: red;">${user_lyrics_input[i]}</span>`;
+    words_incorrect++;
+  }
+}
 
-//const outputElement = document.getElementById("Lyricscroll");
-//let Lyrics_body_Index = 0;
-//function lyricScroll () {
-  //  if (Lyrics_body_Index < lyrics_array)
-    //outputElement.textContent = array[lyrics_array];
-    //Lyrics_body_Index++;
-//}
+// Calculates the total number of words entered
+const words_entered = words_incorrect + words_correct;
+const scorePercentage = (words_correct / words_entered) * 100;
 
-//we need to append the lyrics from the lyrics body given by 
-//the API to the actual_lyrics array and then print that out
-//at the same rate as the song is singing them
+// Join the styled words back into a string
+const styled_lyrics = user_lyrics_input.join(' ');
 
-//if we use a for loop, we can keep track of the 
-//indexs of the array so that the comparison function
-//knows which indexs to compare. 
+// Display the styled lyrics
+const outputElement = document.getElementById('Lyricscroll');
+outputElement.innerHTML = styled_lyrics;
+
+let Lyrics_body_Index = 0;
+function lyricScroll() {
+  if (Lyrics_body_Index < lyrics_array.length) {
+    outputElement.textContent = lyrics_array[Lyrics_body_Index];
+    Lyrics_body_Index++;
+  }
+}
+
+// Call the lyricScroll function to initiate the scrolling
+lyricScroll();
